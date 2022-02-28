27e79302df231c1317f4cdf6e7f631da0967dc94
Valle-ds <83124979+Valle-ds@users.noreply.github.com>
2022-02-28 17:32:04 +0300

    Update CDistNet_config.py

--- configs/CDistNet_config.py
+++ configs/CDistNet_config.py
@@ -40,7 +40,7 @@ train = dict(
     model_dir='models/reconstruct_CDistNet_3_10', 
     num_epochs=16,
     # gpu_device_ids=[1,2,3,4,5,6,7],
-    batch_size=197,  # 4gpu 1800
+    batch_size=120,  # 4gpu 1800
     model=None,
     # model ='models/new_baseline_sem_pos_pos_vis_3_32*128_tps_resnet45_epoch_6/model_epoch_5.pth',
     # current_epoch=6,  # epoch start
@@ -60,7 +60,7 @@ val = dict(
                'data/test',],
     # gt_file=['datasets/NewVersion/val_data/val_data.txt'],
     # gt_file='../dataset/MJ/MJ_valid/',
-    batch_size=197,  # 4gpu 1800
+    batch_size=120,  # 4gpu 1800
     num_worker=16,
 )
 
