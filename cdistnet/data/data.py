2097178e4ba61481ae19c4d6b035e3fbd55f7ba8
Pashteticus <70980291+Pashteticus@users.noreply.github.com>
2022-03-03 21:17:51 +0300

    Update data.py

--- cdistnet/data/data.py
+++ cdistnet/data/data.py
@@ -112,7 +112,6 @@ class TXTDataset(Dataset):
         if self.is_test==False and self.data_aug:
             self.augment_tfs = transforms.Compose([
                 ExtraLinesAugmentation(),
-                CVGaussianNoise(),
                 CVGeometry(degrees=45, translate=(0.0, 0.0), scale=(0.5, 2.), shear=(45, 15), distortion=0.5, p=0.5),
                 CVDeterioration(var=20, degrees=6, factor=4, p=0.25),
                 CVColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1, p=0.25)
@@ -233,7 +232,6 @@ class LMDBDataset(Dataset):
         if self.is_test==False and self.data_aug:
             self.augment_tfs = transforms.Compose([
                 ExtraLinesAugmentation(),
-                CVGaussianNoise(),
                 CVGeometry(degrees=45, translate=(0.0, 0.0), scale=(0.5, 2.), shear=(45, 15), distortion=0.5, p=0.5),
                 CVDeterioration(var=20, degrees=6, factor=4, p=0.25),
                 CVColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.1, p=0.25)
